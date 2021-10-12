0c5affdbb5c4e81ec2bf1558bfdf37a6805cf731
saetan <saetan@live.com>
2021-10-12 22:44:44 +0800

    Updates

---
+++ precache-manifest.57453b75a10f2d3f87eb71ab0dae8766.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f187051cf0b7d6b74a5bf652c107c565",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6381e487d5df04b1fdd0",
+    "url": "/static/css/main.c506c6ee.chunk.css"
+  },
+  {
+    "revision": "98ad9538443952f13f8f",
+    "url": "/static/js/2.eee01206.chunk.js"
+  },
+  {
+    "revision": "6381e487d5df04b1fdd0",
+    "url": "/static/js/main.a8d71fa8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
